690216f24ca7db4d194c73c8f647421e36a982ae
kimsujin <sooojiny@gmail.com>
2023-04-30 21:04:02 +0900

    backend mysql 연동

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,23 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var mysql = require('mysql');
+// Connection 객체 생성 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',   
+  password: 'mysqlPW1!',
+  database: 'instagram_db'  
+});  
+// Connect
+connection.connect(function (err) {   
+  if (err) {     
+    console.error('mysql connection error');     
+    console.error(err);     
+    throw err;   
+  } 
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
