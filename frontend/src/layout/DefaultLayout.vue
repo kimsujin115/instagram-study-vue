0c8d66b682c73103206f62ed14f08b5faa52ddc1
kimsujin <sooojiny@gmail.com>
2023-05-13 21:58:44 +0900

    인스타그램 레이아웃 만들기

--- frontend/src/layout/DefaultLayout.vue
+++ frontend/src/layout/DefaultLayout.vue
@@ -59,41 +59,54 @@
 
         <!-- main section -->
         <div class="contentWrap">
-            <ul class="feedList">
-                <li v-for="feed in 5" :key="feed">
-                    <div class="user">
-                        <div class="profile">
-                            <img src="http://picsum.photos/100" alt="" />
-                        </div>
-                        <span class="name">instar.id</span>
-                        <span class="date">1일</span>
-                    </div>
-                    <div class="feedImg">
-                        <img src="http://picsum.photos/600" alt="" />
-                    </div>
-                    <div class="utill">
-                        <div class="group">
-                            <button>
-                                <i class="far fa-heart"></i>
-                            </button>
-                            <button>
-                                <i class="far fa-comment"></i>
-                            </button>
-                            <button>
-                                <i class="icon far fa-paper-plane"></i>
-                            </button>
-                        </div>
+            <!-- innerCont -->
+            <div class="innerCont">
+                <!-- storyList -->
+                <ul class="storyList">
+                    <li v-for="story in 10" :key="story" class="active">
                         <button>
-                            <i class="fa-regular fa-bookmark"></i>
+                            <div class="img"><img src="http://picsum.photos/200" alt="" /></div>
+                            <span class="user">user.instarid</span>
                         </button>
-                    </div>
-                    <p class="like_num">좋아요 111개</p>
-                    <div class="feedText">
-                        <span class="writer">instar.id</span>
-                        <span class="text">인스타 피드 게시글 내용인스타 피드 게시글 내용인스타 피드 게시글 내용인스타 피드 게시글 내용인스타 피드 게시글 내용인스타 피드 게시글 내용</span>
-                    </div>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+                <ul class="feedList">
+                    <li v-for="feed in 5" :key="feed">
+                        <div class="user">
+                            <div class="profile active">
+                                <img src="http://picsum.photos/100" alt="" />
+                            </div>
+                            <span class="name">instar.id</span>
+                            <span class="date">1일</span>
+                        </div>
+                        <div class="feedImg">
+                            <img src="http://picsum.photos/600" alt="" />
+                        </div>
+                        <div class="utill">
+                            <div class="group">
+                                <button>
+                                    <i class="far fa-heart"></i>
+                                </button>
+                                <button>
+                                    <i class="far fa-comment"></i>
+                                </button>
+                                <button>
+                                    <i class="icon far fa-paper-plane"></i>
+                                </button>
+                            </div>
+                            <button>
+                                <i class="fa-regular fa-bookmark"></i>
+                            </button>
+                        </div>
+                        <p class="like_num">좋아요 111개</p>
+                        <div class="feedText">
+                            <span class="writer">instar.id</span>
+                            <span class="text">인스타 피드 게시글 내용인스타 피드 게시글 내용인스타 피드 게시글 내용인스타 피드 게시글 내용인스타 피드 게시글 내용인스타 피드 게시글 내용</span>
+                            <button class="btnMore">더 보기</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
