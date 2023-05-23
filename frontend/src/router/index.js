4857050e989561e84bfb64658f07336feb367edd
kimsujin <sooojiny@gmail.com>
2023-05-23 23:18:31 +0900

    navigation guard (회원인증이 필요한 페이지에 회원정보가 없을 경우 로그인 페이지로 이동)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Login from '../pages/Login.vue'
 import Home from '../pages/Home.vue'
 import Notifications from '../pages/Notifications.vue'
 import Myfeed from '../pages/MyFeed.vue'
+import store from '../store'
 
 const routes = [
     {path:'/', name:'home', component:Home, meta: { layout:'DefaultLayout', requireAuth:true }},
@@ -17,5 +18,18 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+/* navigation guard */
+router.beforeEach( (to, from, next) => {
+    const currentUser = store.state.user //store에 저장해놓은 현재 유저정보
+    const requireAuth = to.matched.some((requireInfo) => requireInfo.meta.requireAuth);
+
+    if (requireAuth && !currentUser) { //회원인증이 필요한 페이지인데 로그인한 유저가 아니면 로그인 페이지로 이동시킴
+        next('/login');
+    } else {
+        next();
+    }
+})
+
 
 export default router
