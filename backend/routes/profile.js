64873dfd6ee472fc8c253fe95db91606be7e3eef
kimsujin <sooojiny@gmail.com>
2023-06-25 21:17:06 +0900

    fsExtra 호출 시점 변경

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -14,11 +14,9 @@ const storage = multer.diskStorage({
         //유저의 프로필 저장하는 폴더 없을 경우 폴더 생성
         if (!fs.existsSync(`public/images/profile/${userid}`)) { 
             fs.mkdirSync(`public/images/profile/${userid}`)
-        } else {
-            //유저의 프로필 사진은 1개만 저장 할 수 있으므로, 변경할때 기존꺼는 삭제하기
-            fsExtra.emptyDirSync(`public/images/profile/${userid}/`)
-        }
-
+        } 
+       
+        fsExtra.emptyDirSync(`public/images/profile/${userid}/`);  //유저의 프로필 사진은 1개만 저장 할 수 있으므로, 변경할때 기존꺼는 삭제하기
         callback(null, `public/images/profile/${userid}/`);
     },
     //저장되는 파일이름 형식 커스텀 가능
